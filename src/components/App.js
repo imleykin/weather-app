135dba2b360de7cbb73b9478f1404221d6fa17b9
Ilya Leykin <imleykin@gmail.com>
2019-07-16 00:00:27 +0500

    Merger branch 'master'

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import '../style.sass';
 import SearchBar from '../containers/SearchBar';
 
 function App() {
 	return <SearchBar />;
-=======
-import logo from './logo.svg';
-import './App.scss';
-
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
->>>>>>> cab629c4532c6a7ca42b8f44efcaa7a6b51da63c
 }
 
 export default App;

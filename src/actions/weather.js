53131e3877605c09524117d5ef4419e55536d1f7
Ilya Leykin <imleykin@gmail.com>
2019-07-19 00:58:35 +0500

    Switch to metric system

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_KEY = '15e39f24e05f6ea0af88f89dc11e5295';
 const API_KEY = 'c737b5e358ad5a3224496207d8cf23c9';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
 
 export const requestWeather = requestedCity => async dispatch => {
   dispatch(requestWeatherStart());
@@ -12,6 +12,8 @@ export const requestWeather = requestedCity => async dispatch => {
       url: `${ROOT_URL}&q=${requestedCity}`,
       method: 'get',
     });
+
+    console.log(response.data);
 
     const { name: city } = response.data;
 
